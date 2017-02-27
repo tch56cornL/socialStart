548ec548440328724e44b376d8bce6eda9c3bf25
Thomas Hawing <thomas.hawing@gmail.com>
2017-02-27 08:12:19 -0500

    Completed email auth

--- socialStart/AppDelegate.swift
+++ socialStart/AppDelegate.swift
@@ -8,6 +8,9 @@
 
 import UIKit
 import Firebase
+import FBSDKLoginKit
+
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,10 +18,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
+        
         // Override point for customization after application launch.
         
         FIRApp.configure()
+        
+        FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
         
         return true
     }
@@ -43,6 +49,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+    
+    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) ->
+        Bool {
+            return FBSDKApplicationDelegate.sharedInstance().application(application, open: url, sourceApplication: sourceApplication, annotation: annotation)
     }
 
 
--- socialStart/SignInVC.swift
+++ socialStart/SignInVC.swift
@@ -7,9 +7,16 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
 
 class SignInVC: UIViewController {
 
+    @IBOutlet weak var emailField: FancyField!
+    
+    @IBOutlet weak var pwdField: FancyField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,52 @@ class SignInVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func facebookBtnTapped(_ sender: Any) {
+        let facebookLogin = FBSDKLoginManager()
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print("THOMAS: Unable to authenticate with Facebook - \(error)")
+            } else if result?.isCancelled == true {
+                print("THOMAS: User cancelled Facebook Authentication")
+            } else {
+                print("THOMAS: Successfully authenticated with FB")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+        }
+    }
+    
+    func firebaseAuth(_ credential: FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("THOMAS: Unable to authenticate with Firebase - \(error)")
+            } else {
+                print("THOMAS: Successfully authenticated with Firepase")
+            }
+        })
+    }
+    @IBAction func signInTapped(_ sender: Any) {
+        //first we want to make sure that there is text within the emial and password
+        if let email = emailField.text, let pwd = pwdField.text {
+        
+        //will be able to check in with email via firebase
+            FIRAuth.auth()?.signIn(withEmail: email, password: pwd, completion: { (user, error) in
+                if error == nil {
+                    print("THOMAS: User email was authenticated with Firebase")
+                } else {
+                    FIRAuth.auth()?.createUser(withEmail: email, password: pwd, completion: { (user, error) in
+                        if error != nil {
+                            print("THOMAS: Unable to authenticate with Firebase using email")
+                        } else {
+                            print("THOMAS: Successfully authenticated with Firebase")
+                        }
+                    })
+                }
+            })
+        }
+        
+        
+    }
 
 }
 
